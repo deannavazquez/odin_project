92b0e0db2e8597b4782bf61c0493b432a7c6611c
Deanna Vazquez <deanna.vazquez@kin.com>
2025-06-10 13:46:42 -0500

    Adding file to gh repo

---
+++ caesar_cypher.rb
@@ -0,0 +1,16 @@
+# Implement a Caesar cipher that takes in a string and 
+# the shift factor and then outputs the modified string using a right shift:
+# Quick Tips:
+
+# You will need to remember how to convert a string into a number.
+# Don’t forget to wrap from z to a.
+# Don’t forget to keep the same case.
+# The Wikipedia quote discusses a Caesar cipher using a left shift.
+
+#  > caesar_cipher("What a string!", 5)
+ # => "Bmfy f xywnsl!"
+ def caesar_cipher(string, shift)
+  letters = ('a'..'z').to_a
+  
+
+ end
